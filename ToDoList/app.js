d37bf9b3cb438430360a86422f7e947d39a09f26
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-04-11 14:05:36 -0400

    adding Edit button+functionality

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,7 +1,7 @@
 //Importing Packages
 //=========================//=========================
-const express = require ('express');
-const exphbs  = require('express-handlebars');
+const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 
@@ -13,11 +13,11 @@ const app = express();
 //Some Code to get rid of some mongodb_errorsc
 mongoose.Promise = global.Promise;
 //Mongoose Connect
-mongoose.connect('mongodb://127.0.0.1:27017/todolist-dev',{
- //useMongoClient:true
+mongoose.connect('mongodb://127.0.0.1:27017/todolist-dev', {
+    //useMongoClient:true
 })
-.then(() => console.log('... Now Connected to mongo db .... '))
-.catch(err => console.log(err));
+    .then(() => console.log('... Now Connected to mongo db .... '))
+    .catch(err => console.log(err));
 
 //Loading Models
 require('./models/list_item.js');
@@ -30,14 +30,14 @@ const user_model = mongoose.model('user');
 //=========================//=========================
 //Sample
 // app.use((req,res,next) => {
-    
+
 //     next();
 // })
 
 //Express - Handlebars
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
- }));
+}));
 app.set('view engine', 'handlebars');
 
 //Express Body-parser
@@ -47,26 +47,97 @@ app.use(bodyParser.json())
 //Routes
 //=========================//=========================
 //Index
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('index');
 })
 
 //Items
-app.get('/items', (req,res) => {
-    res.render('items');
+app.get('/items', (req, res) => {
+    list_item_model.find({})
+    .sort({date:'desc'})
+    .then(list_items => {
+        res.render('items', {
+            list_items:list_items
+        });
+    });
+    
 })
 
-app.post('/items', (req,res) => {
-    res.send('OK');
+
+
+app.post('/items', (req, res) => {
+    const referal = req.body.referal;
+    console.log(referal);
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "Please Add a Title" });
+    }
+    if (!req.body.description) {
+        errors.push({ text: "Please Add a description" });
+    }
+    if (errors.length>0)
+    {
+        res.render('items/' + referal,{
+            errors:errors,
+            title:req.body.title,
+            description:req.body.description
+        });
+    }
+    else if (referal == 'add')
+    {
+        const new_item ={
+            user_id:1,
+            title:req.body.title,
+            Description:req.body.description
+        }
+        new list_item_model (new_item)
+        .save()
+        .then(
+            res.redirect('/items')
+        )
+        .catch(err => console.log(err));
+    }
+    else if (referal == 'edit')
+    {
+        list_item_model.findOne({
+            _id:req.body.id
+        })
+        .then(list_item_model => {
+            //console.log(req.body);
+            //console.log(list_item_model);
+            list_item_model.title = req.body.title;
+            list_item_model.Description = req.body.description;
+
+            list_item_model.save()
+            .then(list_item_model => {
+                res.redirect('/items')
+            })
+            .catch(err => console.log(err));
+
+        });
+    }
+    
 })
 
+app.get('/items/edit/:id', (req,res) => {
+    list_item_model.findOne({
+        _id:req.params.id
+    })
+    .then(list_item_model => {
+        res.render('items/edit',{
+            id: list_item_model._id,
+            title:list_item_model.title,
+            description:list_item_model.Description
+        });
+    });
+});
 //Add New Items
-app.get('/items/add', (req,res) => {
+app.get('/items/add', (req, res) => {
     res.render('items/add');
 })
 
 //About
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render('about');
 })
 
@@ -75,5 +146,5 @@ app.get('/about', (req,res) => {
 const port = 5000;
 
 app.listen(port, () => {
-    console.log('... app started @ '+ port+ '...');
+    console.log('... app started @ ' + port + '...');
 });
