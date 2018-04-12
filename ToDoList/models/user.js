f669e148ad471fc116a9d4c8dfe074b1b6fdbfda
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-04-12 19:56:41 -0400

    chnaging user_id type from number to string

--- ToDoList/models/user.js
+++ ToDoList/models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 //Schema
 const user_schema = new Schema({
-    id:{
-        type:Number,
+    name:{
+        type:String,
         required:true
     },
     email:{
