9eb1d88aea742d30252365a5001b6800c29578d7
[Hamid] <[heidarzadeh.hamidreza@gmail.com]>
2018-08-18 21:24:36 -0400

    changing NotFound to notFound in handlers

--- ServerToReplaceExpress/apiHandlers.js
+++ ServerToReplaceExpress/apiHandlers.js
@@ -4,7 +4,7 @@ handlers.notFound = function(data,callback){
     callback(404,{"ERROR":"Requested Page Doesn't Exist"});
 };
 
-handlers.notFound = function(data,callback){
+handlers.ping = function(data,callback){
     callback(200,{"SUCCESS":"The Application is UP!"});
 };
 
