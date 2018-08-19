40232f66ba4e881e5ac0a78a43e4d265d1db2a7e
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-19 19:48:17 -0400

    adding notFound/Ping handler

---
+++ MongodbNoMongoose/apiHandler.js
@@ -0,0 +1,13 @@
+var handlers = {};
+
+handlers.notFound = function(data,callback){
+    var message = {"ERROR":"Requested URL not There!"};
+    callback(404,message);
+};
+handlers.ping = function(data,callback){
+    var message = {"SUCCESS":"The App is UP!"};
+    callback(200,message);
+};
+handlers.users = function(data,callback){};
+
+module.exports = handlers;
